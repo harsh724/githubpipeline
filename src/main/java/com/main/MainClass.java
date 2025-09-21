54f25b8a1b0c7772aff9c01ce43f668aaa0c23aa
harsh724 <vharsh072@gmail.com>
2025-09-21 14:23:30 +0530

    added syntactical correct code

--- src/main/java/com/main/MainClass.java
+++ src/main/java/com/main/MainClass.java
@@ -21,14 +21,28 @@ public final class MainClass {
     public static void main(final String[] args) throws Exception {
         System.out.println("here goes the pipeline");
     }
+
+    /**
+     * Sample test method.
+     *
+     * @param param1 first parameter
+     * @param param2 second parameter
+     */
     @Test(dataProvider = "dp")
-    public void test1(String param1, String param2){
-        System.out.println("Test1: "+param1+" and "+param2);
+    public void test1(final String param1, final String param2) {
+        System.out.println("Test1: " + param1 + " and " + param2);
     }
 
+    /**
+     * Provides test data for {@link #test1(String, String)}.
+     *
+     * @return 2D array of test data
+     */
     @DataProvider(name = "dp")
-    public Object[][] dataProvider(){
-        return new Object[][] {{"hello", "world"}
-                , {"testcase", "second"}};
+    public Object[][] dataProvider() {
+        return new Object[][] {
+                {"hello", "world"},
+                {"testcase", "second"}
+        };
     }
 }
